0cc6a5adfbced1d5c4b2aa5038d3e0c2ae981b26
leomue1 <leo.muehlstrasser@tum.de>
2025-03-07 11:43:51 +0100

    Modified perform_relay_test function to perform_calibration function

--- Calibration.py
+++ Calibration.py
@@ -6,96 +6,102 @@ is triggered and the angle that the camera's coordinate system is rotated by com
 coordinate system and writes the values into the config.ini file for further processing.
 """
 
-
-import os
+import numpy as np
 import time
 import cv2 as cv
 from picamera2 import Picamera2
 import calc
 import relay_handling
 import config_loader
+import configparser
 
-
+#Initialize outside classes
 config=config_loader.configuration()
-
-
-def capture():
-    # Initialize Picamera2
-    picam = Picamera2()
-
-    # Specify the folder path
-    folder_path = "calibration_pics"
-
-    # Check if the folder exists, and if not, create it
-    if not os.path.exists(folder_path):
-        os.makedirs(folder_path)
-
-    # Picam Configuration
-    camera_config = picam.create_video_configuration(
-        main={'format': 'RGB888', "size": (4056, 3040)},
-        buffer_count=7,
-    )
-    picam.configure(camera_config)
-
-    # Start the camera
-    picam.start()
-
-    # Display the live feed
-    while True:
-        img = picam.capture_array()
-        cv.namedWindow('Camera Output', cv.WINDOW_NORMAL)
-        cv.setWindowProperty('Camera Output', cv.WND_PROP_FULLSCREEN, cv.WINDOW_FULLSCREEN)
-        cv.imshow('Camera Output', img)
-        key = cv.waitKey(1)
-        if key != -1:  # Exit loop on key press
-            break
-
-    # Close the display window
-    cv.destroyAllWindows()
-
-    # Capture and save a single image
-    img = picam.capture_array()
-    print("Image captured.")
-    # calc.calculation.preprocessing(img)
-    image_path = os.path.join(folder_path, 'calibration_img.png')
-    cv.imwrite(image_path, img)
-
-    print(f"Image saved at: {image_path}")
-
-    # Cleanup
-    cv.destroyAllWindows()
-
+log = calc.log(config)
+guide=relay_handling.guide(log, config)
+clc = calc.calculation(config)
+picam=Picamera2()
 
 relay_pins = config.relay_pins
-#
-calib_relay_pins = [relay_pins[0], relay_pins[2], relay_pins[1], relay_pins[3]]
-# Read the moon's initial starting position
-calib_img = capture()
-processed_calib_img = calc.calculation.preprocessing(calib_img)
-starting_pos = calc.calculation.moonposition(processed_calib_img)
-print(starting_pos)
 
-# For each moving direction (Ra+, Ra-, Dec+, Dec-): Trigger the relay for n seconds, read end position and move back
-"""
-for pin in calib_relay_pins:
-    relay_handling.guide.pulse(pin, 1, 5.0, 0.1)
-    capture()
+def perform_calibration(moving_time):
+    """ Outputs the image of the camera to the display, marking a detected target. The
+        user then needs to confirm that the correct target is found by pressing the button.
+        The function now iterates over the relay pins, activating them in pulses. After each
+        pin the target location is checked again and the deviation is printed to the console.
+        Can be skipped by pressing any key.
+
+        Returns: None
+    """
+
+    deviations = []
+
+    # Capture loop for camera images and target detection and marking
+    while True:
+        cv.namedWindow('Camera Output', cv.WINDOW_NORMAL)
+        cv.setWindowProperty(
+            'Camera Output', cv.WND_PROP_FULLSCREEN, cv.WINDOW_FULLSCREEN)
+
+        org_image = picam.capture_array()
+        processed = clc.preprocessing(org_image)
+
+        (target_x, target_y, _) = clc.moonposition(processed)
+
+        marked = clc.targetmarkers(
+            target_x,
+            target_y,
+            _,
+            target_x,
+            target_y,
+            (0, 0),
+            org_image,
+            "Press any key to skip relay test.\nPress Button if correct Target is found."
+        )
+
+        cv.imshow('Camera Output', marked)
+        key = cv.waitKey(1)
+
+        if key != -1:
+            return
+
+        # Confirm correct target
+        if guide.button_is_pressed():
+            cv.destroyAllWindows()
+            print("Relay Testing in Progress...\nThis will take 40s")
+            time.sleep(1)
+            break
+
+    # Get target position again
+    org_image = picam.capture_array()
+    processed = clc.preprocessing(org_image)
+    (target_x, target_y, _) = clc.moonposition(processed)
+
+    # Iterate over each pin, checking for target between activations
+    for pin, direction in zip(guide.relay_pins, ["right", "left", "down", "up"]):
+        org_image = picam.capture_array()
+        processed = clc.preprocessing(org_image)
+
+        # Obtain initial test position
+        (x_0, y_0, r_0) = clc.moonposition(processed)
+
+        print(f"Testing pin {pin} ({direction})...")
+        guide.pulse(pin, moving_time, 2, 0.2)
+        
+        org_image = picam.capture_array()
+        processed = clc.preprocessing(org_image)
+        # Obtain final test position
+        (x, y, r) = clc.moonposition(processed)
+        deviation = clc.get_deviation((x, y), (x_0, y_0))
+        deviations.append(deviation)
+        print(f"Detected deviation: {deviation}")
+    print("\nPress Button to continue")
+    while True:
+        if guide.button_is_pressed():
+            break
+
+    return deviations
 
 
-    relay_handling.guide.pulse(pin+2, 1, 5.0, 0.1)
-"""
-    # Read starting position again and compare as a fail-safe
 
-    # Compute travelled distance s
-
-    # Compute pixels per second
-
-    # Compute rotation angle alpha
-
-
-# Average results
-
-
-# Compute pulse_multiplier and write it into the config.ini file
 
 
